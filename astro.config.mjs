36fe6d7137e79c8d87715cc553c8956b60c5a6bd
silviofrancoms10 <silviofrancoms@gmail.com>
2026-05-21 21:11:26 -0400

    chore: remove tailwindcss dependency and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
